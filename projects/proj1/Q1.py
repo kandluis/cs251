3d57ebce7c3004efad4921a669e90fd168ddd29d
Luis Perez <luis.perez.live@gmail.com>
2019-10-05 16:47:30 -0700

    Finished Pset1 and Q1 for project

---
+++ projects/proj1/Q1.py
@@ -0,0 +1,72 @@
+from bitcoin.core.script import *
+from bitcoin.wallet import CBitcoinSecret, P2PKHBitcoinAddress
+
+from lib.utils import *
+from lib.config import (my_private_key, my_public_key, my_address,
+                    faucet_address, network_type)
+
+
+def P2PKH_scriptPubKey(address):
+    ######################################################################
+    # TODO: Complete the standard scriptPubKey implementation for a
+    # PayToPublicKeyHash transaction
+    return [
+        OP_DUP, OP_HASH160, address, OP_EQUALVERIFY, OP_CHECKSIG
+    ]
+    ######################################################################
+
+
+def P2PKH_scriptSig(txin, txout, txin_scriptPubKey, private_key, public_key):
+    signature = create_OP_CHECKSIG_signature(txin, txout, txin_scriptPubKey,
+                                             private_key)
+    ######################################################################
+    # TODO: Complete this script to unlock the BTC that was sent to you
+    # in the PayToPublicKeyHash transaction.
+    return [
+        signature, public_key
+    ]
+    ######################################################################
+
+def send_from_P2PKH_transaction(amount_to_send,
+                                txid_to_spend,
+                                utxo_index,
+                                txout_scriptPubKey,
+                                sender_private_key,
+                                network):
+
+    sender_public_key = sender_private_key.pub
+    sender_address = P2PKHBitcoinAddress.from_pubkey(sender_public_key)
+
+    txout = create_txout(amount_to_send, txout_scriptPubKey)
+
+    txin_scriptPubKey = P2PKH_scriptPubKey(sender_address)
+    txin = create_txin(txid_to_spend, utxo_index)
+    txin_scriptSig = P2PKH_scriptSig(txin, txout, txin_scriptPubKey,
+        sender_private_key, sender_public_key)
+
+    new_tx = create_signed_transaction(txin, txout, txin_scriptPubKey,
+                                       txin_scriptSig)
+
+    return broadcast_transaction(new_tx, network)
+
+
+if __name__ == '__main__':
+    ######################################################################
+    # TODO: set these parameters correctly
+    amount_to_send = 0.00263379 - 0.001 # amount of BTC in the output you're splitting minus fee
+    txid_to_spend = (
+        'd55335bb08a67bf7eb456ffbed6d548efe109740e590613c34003636546b20ec')
+    utxo_index = 0 # index of the output you are spending, indices start at 0
+    ######################################################################
+
+    txout_scriptPubKey = P2PKH_scriptPubKey(faucet_address)
+    response = send_from_P2PKH_transaction(
+        amount_to_send,
+        txid_to_spend,
+        utxo_index,
+        txout_scriptPubKey,
+        my_private_key,
+        network_type,
+    )
+    print(response.status_code, response.reason)
+    print(response.text)
